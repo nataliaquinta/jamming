baa701fc75bcad328d7084174bb9fe1032137c6d
nataliaquinta <natalia.s.quita@gmail.com>
2018-02-26 18:11:16 +0100

    first commit

---
+++ src/components/SearchBar.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      term: ''
+    };
+
+    this.handleTermChange = this.handleTermChange.bind(this);
+    this.search = this.search.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+  }
+
+  handleTermChange(event) {
+    this.setState({term: event.target.value});
+  }
+
+  handleKeyPress(event) {
+    if(event.key === 'Enter') {
+      this.props.onSearch(this.state.term);
+    }
+  }
+
+  search() {
+    this.props.onSearch(this.state.term);
+  }
+
+  render() {
+    return (
+      <div className="SearchBar">
+        <input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange} onKeyPress={this.handleKeyPress} />
+        <a onClick={this.search}>SEARCH</a>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
---
+++ src/components/SearchResults.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Tracklist from './Tracklist';
+
+class SearchResults extends React.Component {
+  render() {
+    return (
+      <div className="SearchResults">
+        <h2>Results</h2>
+        <Tracklist tracks={this.props.searchResults} onAdd={this.props.onAdd} />
+      </div>
+    );
+  }
+}
+
+export default SearchResults;
